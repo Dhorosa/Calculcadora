f2ec647e1381010779a00e131dcd049059a07b84
DaniellOlliva <53453696+Dhorosa@users.noreply.github.com>
2020-07-19 12:35:39 -0300

    Aula de Operadores

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -2,6 +2,7 @@ class CalcController {
 
     constructor(){
 
+         this._operation = [];
          this._locale = 'pt-BR';
          this._displayCalcEl = document.querySelector('#display');
          this._dateEl = document.querySelector('#data');
@@ -24,8 +25,117 @@ class CalcController {
         events.split(' ').forEach(event=>{
             element.addEventListener(event, fn, false);
         })
+    }
+
+    clearAll(){
+        this._operation = [];
+    }
+    cancelEntry(){
+        this._operation.pop();
+    }
+    
+    getLastOparetion(){
+
+        return this._operation[this._operation.length - 1];
 
 
+    }   
+    isOperatior(value){
+       
+       return (['+','-','/','*','%'].indexOf(value) > -1);
+
+    }
+
+    addOperation(value){
+        
+        if (isNaN(this.getLastOparetion())){
+
+            if (this.isOperatior(value)){
+
+                this._operation[this._operation.length - 1] = value;
+
+            }else if(isNaN(value) ){
+                console.log(value); 
+           
+            } else{
+                this._operation.push(value);
+            }   
+            
+
+        }else {
+            let newValue = this.getLastOparetion().toString() + value.toString();
+            this._operation.push(newValue);
+        }
+        console.log(this._operation);
+    
+    
+        
+    }
+
+    setError(){
+        this.displayCalc = "Error";
+    }
+
+
+
+    execBtn(value){
+
+        switch(value){
+
+            case 'ac':
+                this.clearAll();
+                break;
+
+            case 'ce':
+                this.cancelEntry();
+                break;
+
+            case 'soma':
+                this.addOperation('+');
+                break;
+
+            case 'subtracao':
+                this.addOperation('-');
+                break;
+
+            case 'divisao':
+                this.addOperation('/');
+                break;
+
+            case 'multiplicacao':
+                this.addOperation('*');
+                break;
+
+            case 'porcento':
+                this.addOperation('%');
+                break;
+
+            case 'igual':
+                
+                break;
+            case 'ponto':
+                this.addOperation('.');
+                break;
+
+
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                this.addOperation(parseInt(value));
+                break;
+                
+            default :
+                this.setError();
+                break
+            
+        }
     }
 
 
